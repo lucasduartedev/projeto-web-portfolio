e51b828ad9a5e906735a3e85e9f63134b05b99a1
Lucas Duarte <lucasduartedev@gmail.com>
2026-01-19 15:41:03 -0300

    Estilo botões: cadastro e listagem de tarefas

--- js/lista-tarefas.js
+++ js/lista-tarefas.js
@@ -1,18 +1,21 @@
 
 const criarTarefa = () => {
 
-    const nomeNovaTarefa = document.querySelector('[data-input-nova-tarefa]').value;
+    const nomeNovaTarefa = document.querySelector('[data-input-nova-tarefa]');
     const tarefasListagem = document.querySelector('[data-tarefa-listagem]');
 
     const tarefa = document.createElement('li');
     tarefa.classList.add('listagem-tarefas__item');
     tarefa.innerHTML =
-    `<p class="listagem-tarefas__nome-tarefa">${nomeNovaTarefa}</p>
+    `<p class="listagem-tarefas__nome-tarefa">${nomeNovaTarefa.value}</p>
     <div class="listagem-tarefas__botoes">
     <button class="btn-concluir">Concluir</button> <button class="btn-deletar">Deletar</button>
     <div>`;
     
     tarefasListagem.appendChild(tarefa);
+
+    nomeNovaTarefa.value = '';
+    nomeNovaTarefa.focus();
 
 }
 
